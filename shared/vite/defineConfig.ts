683df3daeda334b291bdaeae31f8061241203b46
yujeongJeon <workingnewjeong@gmail.com>
2024-09-23 01:03:52 +0900

    @ndive/vite PureESM으로 빌드하도록 구현

---
+++ shared/vite/defineConfig.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import path from 'node:path'
+
+import browserslistToEsbuild from 'browserslist-to-esbuild'
+import {defineConfig} from 'vite'
+
+const normalizeOutput = (exportPath: string | {default: string}) => {
+    return typeof exportPath === 'string' ? exportPath : exportPath?.default
+}
+
+const SUPPORT_TARGETS = browserslistToEsbuild()
+
+export default ({pkg, entry}: {pkg: Record<string, any>; entry: {index: string} & Record<string, string>}) => {
+    const outputPath = pkg.exports ? pkg.exports['.'] : pkg.main
+
+    const buildOutput = typeof outputPath === 'object' ? normalizeOutput(outputPath.import) : outputPath
+
+    const outDir = path.dirname(buildOutput)
+
+    return defineConfig({
+        build: {
+            outDir,
+            lib: {
+                entry,
+                formats: ['es'],
+            },
+            rollupOptions: {
+                external: [...Object.keys(pkg.peerDependencies || {}), ...Object.keys(pkg.dependencies || {})].flatMap(
+                    (dep) => [dep, new RegExp(`^${dep}/.*`)],
+                ),
+                output: [
+                    {
+                        format: 'es',
+                        dir: outDir,
+                        entryFileNames: `[name]${path.extname(buildOutput)}`,
+                        preserveModulesRoot: path.dirname(entry.index),
+                        preserveModules: true,
+                        interop: 'esModule',
+                    },
+                ],
+            },
+            target: SUPPORT_TARGETS,
+        },
+    })
+}
