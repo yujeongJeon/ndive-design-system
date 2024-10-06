f7aa000714b416432a5b7ed3c057687e2877e9bc
yujeongJeon <workingnewjeong@gmail.com>
2024-10-06 21:38:54 +0900

    remove color.type.ts

--- packages/design-tokens/src/types/color.type.ts
+++
@@ -1,9 +0,0 @@
-import {IFrame} from './figma.type'
-
-export type TColorSetName = 'Grayscale' | 'Sub' | 'Brand' | 'Background' | 'Border'
-
-export type TColorSetFrame = IFrame<TColorSetName>
-
-export type TColorDocumentFrame = IFrame<'Color', TColorSetName, string, IFrame<TColorSetName>>
-
-export type TColorReturnType = Record<Uppercase<TColorSetName>, Record<string, string>>
