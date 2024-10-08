46c552b3743ecf7effecb2889c5566255ae7d0ef
yujeongJeon <workingnewjeong@gmail.com>
2024-10-09 01:15:37 +0900

    Sample 컴포넌트 제거

--- packages/design-components/src/index.ts
+++ packages/design-components/src/index.ts
@@ -1,7 +1,6 @@
-export {default as Sample} from '$/components/Sample'
-
 // Button
 export {default as ButtonPrimary} from '$/components/button/ButtonPrimary'
 export {default as ButtonSub} from '$/components/button/ButtonSub'
 
+// Icon
 export * from '$/components/icons'
