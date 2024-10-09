b810bc3e941e2f687ecc744b683271fd1b16f5e5
yujeongJeon <workingnewjeong@gmail.com>
2024-10-10 01:02:43 +0900

    ButtonSub 구현, 스토리북 추가

--- packages/design-components/src/components/button/ButtonSub.tsx
+++ packages/design-components/src/components/button/ButtonSub.tsx
@@ -1,9 +1,30 @@
+import classnames from 'classnames/bind'
+
 import {CommonButtonProps} from '$/types/button.types'
 
-export default function ButtonSub({text, onClick, attributes, disabled}: CommonButtonProps) {
+import styles from './ButtonSub.module.scss'
+
+const cx = classnames.bind(styles)
+
+export default function ButtonSub({text, onClick, attributes, disabled, fillType, size, icon}: CommonButtonProps) {
+    const hasIcon = !!icon
+
+    const IconComponent = icon?.component
+
     return (
-        <button onClick={onClick} disabled={disabled} {...attributes}>
+        <button
+            className={cx('button', fillType, `size-${size}`)}
+            onClick={onClick}
+            disabled={disabled}
+            {...attributes}
+        >
+            {hasIcon && icon.direction === 'front' && (
+                <span className={cx('icon', icon.direction)}>{IconComponent}</span>
+            )}
             {text}
+            {hasIcon && icon.direction === 'back' && (
+                <span className={cx('icon', icon.direction)}>{IconComponent}</span>
+            )}
         </button>
     )
 }
