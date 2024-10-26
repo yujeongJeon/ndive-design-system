bc6ecc7d43d3e219046423d7f6d8d11aaa44dd80
yujeongJeon <workingnewjeong@gmail.com>
2024-10-26 21:26:19 +0900

    /examples/shopping-web 보완

--- examples/shopping-web/src/Container.tsx
+++ examples/shopping-web/src/Container.tsx
@@ -1,22 +1,23 @@
 import {ButtonPrimary, ModalNotice} from '@ndive/design-components'
 
-export default function Container() {
+export default function Container({isShow, onClose}: {isShow: boolean; onClose: () => void}) {
     return (
         <ModalNotice
-            isShow={false}
-            title="제목"
+            isShow={isShow}
+            title="장바구니에 상품을 추가했습니다."
             content={
                 <>
                     <ButtonPrimary text="자세히 보기" fillType="line" color="mainYellow" size="small" />
                 </>
             }
             button={{
-                text: '확인',
+                text: '더 살펴보기',
                 fillType: 'fill',
                 color: 'mainGreen',
                 size: 'medium',
+                onClick: onClose,
             }}
-            onClose={() => {}}
+            onClose={onClose}
         />
     )
 }
