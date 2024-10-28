682a801286ae0ab867c962ca17524064b6475a13
yujeongJeon <yujeongJeon@users.noreply.github.com>
2024-10-28 14:21:35 +0000

    Merge pull request #31 from yujeongJeon/fix/vite
    번들러 코드 수정 6c46279c5535ea980aa028b230e683394d06b51b

---
+++ assets/ButtonPrimary.stories-D2dRpqTz.js
@@ -0,0 +1,44 @@
+import{j as i}from"./jsx-runtime-DEdD30eg.js";import{j as l}from"./ButtonPrimary-BfRps1L6.js";import{o as r}from"./IconAdd-CDUPXIWo.js";import"./index-RYns6xqu.js";import"./bind-DUXHF5Y5.js";const d={title:"Components/Button",component:l},n={args:{text:"버튼",color:"mainGreen",fillType:"fill",size:"small",disabled:!1,icon:{direction:"front",component:i.jsx(r,{width:16,height:16,fill:"#fff"})},isWide:!1},argTypes:{color:{control:{type:"select"},options:["mainGreen","mainYellow","red","teal"]},fillType:{control:{type:"radio"},options:["fill","line"]},size:{control:{type:"radio"},options:["small","medium","large"]},disabled:{control:{type:"boolean"}},isWide:{control:{type:"boolean"}}}};var o,e,t;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    text: '버튼',
+    color: 'mainGreen',
+    fillType: 'fill',
+    size: 'small',
+    disabled: false,
+    icon: {
+      direction: 'front',
+      component: <IconAdd width={16} height={16} fill={'#fff'} />
+    },
+    isWide: false
+  },
+  argTypes: {
+    color: {
+      control: {
+        type: 'select'
+      },
+      options: ['mainGreen', 'mainYellow', 'red', 'teal']
+    },
+    fillType: {
+      control: {
+        type: 'radio'
+      },
+      options: ['fill', 'line']
+    },
+    size: {
+      control: {
+        type: 'radio'
+      },
+      options: ['small', 'medium', 'large']
+    },
+    disabled: {
+      control: {
+        type: 'boolean'
+      }
+    },
+    isWide: {
+      control: {
+        type: 'boolean'
+      }
+    }
+  }
+}`,...(t=(e=n.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const f=["ButtonPrimary"];export{n as ButtonPrimary,f as __namedExportsOrder,d as default};
