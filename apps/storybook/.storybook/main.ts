bba8783c33f1d62d025b767445fef64eb6b408ef
yujeongJeon <workingnewjeong@gmail.com>
2024-11-11 13:26:04 +0900

    [storybook] vite.config.ts

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,11 +1,16 @@
 import type {StorybookConfig} from '@storybook/react-vite'
+import path from 'node:path'
 
 export default {
     stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
     framework: {
         name: '@storybook/react-vite',
-        options: {},
+        options: {
+            builder: {
+                viteConfigPath: path.resolve(__dirname, '../vite.config.ts'),
+            },
+        },
     },
     core: {
         disableTelemetry: true,
