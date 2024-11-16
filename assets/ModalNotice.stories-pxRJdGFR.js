48ef13059dd8d7b95e411d3dd4b6c39058d5d3e5
yujeongJeon <yujeongJeon@users.noreply.github.com>
2024-11-16 19:01:12 +0000

    Merge pull request #59 from yujeongJeon/hotfix/tsup
    tsup@8.3.5 d22b70c835d069604d67540907d77ce8f12b9669

---
+++ assets/ModalNotice.stories-pxRJdGFR.js
@@ -0,0 +1,21 @@
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{f as x}from"./bind-DUXHF5Y5.js";import{j as h}from"./ButtonPrimary-BfRps1L6.js";import{l as f,m as j,n as S,c as w,i as b,a as y,b as N}from"./Modal.module.scss-Dd2UIpIy.js";import"./index-RYns6xqu.js";const s=x.bind(y);function g({title:o,content:a,isShow:t,onClose:c,button:m}){const{isRendered:d,unmount:p}=j(),u=()=>{p()};return d?n.jsxs("div",{className:s("article"),onTransitionEnd:u,children:[t&&n.jsxs("div",{className:s("container"),children:[n.jsx(S,{text:o}),n.jsx(w,{children:a}),n.jsx(b,{children:n.jsx(h,{isWide:!0,...m})})]}),n.jsx(N,{onClick:c,isShow:t})]}):null}function C(o){return n.jsx(f,{isShow:o.isShow,children:n.jsx(g,{...o})})}const v={title:"Components/Modal",component:C},e={args:{title:"로그인이 필요한 서비스입니다.",content:"로그인 후 간편하게 이용하세요!",isShow:!0,onClose:()=>{},button:{text:"로그인",fillType:"fill",color:"mainGreen",size:"medium"}},argTypes:{isShow:{control:{type:"boolean"}}}};var i,r,l;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    title: '로그인이 필요한 서비스입니다.',
+    content: '로그인 후 간편하게 이용하세요!',
+    isShow: true,
+    onClose: () => {},
+    button: {
+      text: '로그인',
+      fillType: 'fill',
+      color: 'mainGreen',
+      size: 'medium'
+    }
+  },
+  argTypes: {
+    isShow: {
+      control: {
+        type: 'boolean'
+      }
+    }
+  }
+}`,...(l=(r=e.parameters)==null?void 0:r.docs)==null?void 0:l.source}}};const z=["ModalNotice"];export{e as ModalNotice,z as __namedExportsOrder,v as default};
